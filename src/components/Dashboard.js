4f03b5c0c7e1d056234d96e7cb812623c3e296ae
launchpad5682 <launchpad5682@gmail.com>
2021-09-29 19:43:47 +0530

    Refactored modal overlay form to new component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,29 +1,21 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import { useAuth } from "../context/AuthContext";
 import { useFireStore } from "../hooks/useFirestore";
 import SearchBox from "./SearchBox";
 import { useHistory } from "react-router-dom";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import ThemeSwitch from "./ThemeSwitch";
+import ModalOverlay from "./ModalOverlay";
+import { useModalOverlay } from "../context/ModalOverlayContext";
 
 function Dashboard() {
-  const { loading, currentUser, uploadData, deleteData, logout } = useAuth();
+  const { loading, currentUser, deleteData, logout } = useAuth();
   const { docs } = useFireStore("gallery");
-  const [modalOverlay, setModalOverlay] = useState(false);
-  const nameRef = useRef();
-  const urlRef = useRef();
+  const { modalOverlay, setModalOverlay } = useModalOverlay();
   const history = useHistory();
 
-  function submitHandler(e) {
-    e.preventDefault();
-    uploadData(nameRef.current.value, urlRef.current.value);
-    setModalOverlay(false);
-    nameRef.current.value = null;
-    urlRef.current.value = null;
-  }
-
   return (
-    <div className="w-full flex flex-col dark:bg-black">
+    <div className="w-full flex flex-col dark:bg-black min-h-screen">
       {loading && JSON.stringify(currentUser.uid)}
       <div className="flex xs:flex-col xs:justify-center lg:flex-row md:justify-between xs:px-10 lg:px-24 pt-5">
         <div className="flex xs:flex-col md:flex-row items-center">
@@ -51,63 +43,15 @@ function Dashboard() {
           </button>
         </div>
       </div>
-      {modalOverlay && (
-        <div className="bg-black bg-opacity-50 fixed inset-0 flex justify-center items-center overscroll-y-none">
-          <div className=" bg-white flex flex-col py-2 px-4 rounded-lg">
-            <div className="flex justify-between">
-              <span className="text-center">Add an image</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6 cursor-pointer hover:bg-gray-300 p-1 rounded-full"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                onClick={() => setModalOverlay(false)}
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </div>
-            <form onSubmit={submitHandler} className="space-y-4 py-6">
-              <label>Name</label>
-              <div>
-                <input
-                  type="text"
-                  ref={nameRef}
-                  className="w-full focus:outline-none border-gray-300 border-solid border-2 h-8 rounded-lg px-3 py-4 focus:border-blue-500"
-                ></input>
-              </div>
-              <label>URL</label>
-              <div>
-                <input
-                  type="text"
-                  ref={urlRef}
-                  className="w-full focus:outline-none border-gray-300 border-solid border-2 h-8 rounded-lg px-3 py-4 focus:border-blue-500"
-                ></input>
-              </div>
-              <button
-                type="submit"
-                className="bg-blue-600 p-2 text-yellow-50 rounded-md min-w-full"
-              >
-                Submit
-              </button>
-            </form>
-          </div>
-        </div>
-      )}
+      {modalOverlay && <ModalOverlay />}
       <ResponsiveMasonry className="pt-12 xs:px-10 lg:px-24">
         <Masonry>
           {docs
             ? docs.map((image) => {
-                //console.log(image.uid);
                 return (
                   <div
                     key={image.uid}
-                    className="xs:w-72 lg:w-96 h-auto rounded-xl mb-10 shadow-2xl dark:bg-white"
+                    className="xs:w-full lg:w-5/6 h-auto rounded-xl mb-10 shadow-2xl dark:bg-white"
                   >
                     <img
                       src={image.url}
