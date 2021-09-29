3b85e583523031a2b53e278daa3b5ec2d5054b6e
launchpad5682 <launchpad5682@gmail.com>
2021-09-29 19:49:29 +0530

    Unused import removed

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { useAuth } from "../context/AuthContext";
 import { useFireStore } from "../hooks/useFirestore";
 import SearchBox from "./SearchBox";
